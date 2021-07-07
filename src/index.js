5114365609887dd1d72310c7b9d58f5dece33600
Kirill Kozedub <dev@kozedub.ru>
2021-07-07 16:46:19 +0300

    refactor: change conditions

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
 exports.min = function min (array) {
-  if (!Array.isArray(array) || array.length === 0) {
-    return 0;
-  };
-  // Вычисление min-значения
-  let result = array[0];
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i] < result) {
-      result = array[i];
+  if (Array.isArray(array) && array.length > 0) {
+    // Вычисление min-значения
+    let result = array[0];
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] < result) {
+        result = array[i];
+      }
     }
+    return result;
   }
-  return result;
+  return 0;
 }
 
 exports.max = function max (array) {
-  if (typeof array === 'undefined' || array.length === 0) {
-    return 0;
-  };
-  // Вычисление max-значения
-  let result = array[0];
-  for (let i = 0; i < array.length; i += 1) {
-    if (array[i] > result) {
-      result = array[i];
+  if (Array.isArray(array) && array.length > 0) {
+    // Вычисление max-значения
+    let result = array[0];
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] > result) {
+        result = array[i];
+      }
     }
+    return result;
   }
-  return result;
+  return 0;
 }
 
+
 exports.avg = function avg (array) {
-  if (!Array.isArray(array) || array.length === 0) {
-    return 0;
-  };
-  // Вычисление среднего значения
-  let result = 0;
-  for (let i = 0; i < array.length; i += 1) {
-    result += array[i];
+  if (Array.isArray(array) && array.length > 0) {
+    // Вычисление среднего значения
+    let result = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      result += array[i];
+    }
+    return (result / array.length);
   }
-  return (result / array.length);
+  return 0;
 }
